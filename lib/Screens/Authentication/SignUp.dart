e7db8f9c43012fd8d06cbe6296b1b09391cd9dfd
Tarunbhati100 <70061858+Tarunbhati100@users.noreply.github.com>
2020-11-21 14:47:08 -0800

    SignUp page Created

--- lib/Screens/Authentication/SignUp.dart
+++ lib/Screens/Authentication/SignUp.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class SignUp extends StatelessWidget {
   Function toggleCallback;
   Function backCallback;
-  SignUp({this.toggleCallback,this.backCallback});
+  SignUp({this.toggleCallback, this.backCallback});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,87 +21,166 @@ class SignUp extends StatelessWidget {
       ),
       body: Container(
         padding: EdgeInsets.all(30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-              width: double.infinity,
-              child: Column(
+        child: SingleChildScrollView(
+                  child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Create an account. It's free",
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  labelText: "Email",
+                  alignLabelWithHint: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                onChanged: null,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  labelText: "Password",
+                  alignLabelWithHint: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                onChanged: null,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Confirm Password",
+                  labelText: "Confirm Password",
+                  alignLabelWithHint: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                onChanged: null,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Codechef Handle",
+                  labelText: "Codechef Handle",
+                  alignLabelWithHint: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                onChanged: null,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Codeforces Handle",
+                  labelText: "Codeforces Handle",
+                  alignLabelWithHint: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                onChanged: null,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "HakerRank Handle",
+                  labelText: "HackerRank Handle",
+                  alignLabelWithHint: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                onChanged: null,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "GitHub Handle",
+                  labelText: "GitHub Handle",
+                  alignLabelWithHint: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                onChanged: null,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                color: Colors.yellow,
+                onPressed: () {},
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(fontSize: 50),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Text(
-                    "Login",
-                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                    "Already have an Account? | ",
+                    textAlign: TextAlign.end,
                   ),
-                  Text(
-                    "Login to your account",
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
+                  FlatButton(
+                      onPressed: toggleCallback,
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ))
                 ],
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                hintText: "Email",
-                labelText: "Email",
-                alignLabelWithHint: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                ),
-              ),
-              onChanged: null,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "Password",
-                labelText: "Password",
-                alignLabelWithHint: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                ),
-              ),
-              onChanged: null,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            RaisedButton(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-              color: Colors.yellow,
-              onPressed: () {},
-              child: Text(
-                "Sign Up",
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  "Already have an Account? | ",
-                  textAlign: TextAlign.end,
-                ),
-                FlatButton(
-                    onPressed: toggleCallback,
-                    child: Text(
-                      "Log In",
-                      style: TextStyle(
-                        color: Colors.blue,
-                      ),
-                    ))
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
